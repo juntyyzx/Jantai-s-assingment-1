795cb8fdd9e9a006fe969a56095ebc75eec7b6c1
juntyyzx <juntuy242010@gmail.com>
2026-04-14 13:58:21 +0600

    Refactor text input and counting logic
    Refactored code to improve readability and structure.

--- lets,nums_counter.py
+++ lets,nums_counter.py
@@ -1,17 +1,17 @@
-print('Введите ваш текст:',end='')
-text=input()
-let_count=0
-num_count=0
-other_symbol_count=0
-letters='QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбюёЁ'
-numbers='1234567890'
+print('Введите ваш текст:',end = '')
+text = input()
+let_count = 0
+num_count = 0
+other_symbol_count = 0
+letters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбюёЁ'
+numbers = '1234567890'
 for i in range(len(text)):
     if text[i] in letters:
-        let_count+=1
+        let_count += 1
     elif text[i] in numbers:
-        num_count+=1
+        num_count += 1
     else:
-        other_symbol_count+=1
-print('Колличество цифр в вашем тексте:',num_count)
-print('Колличество букв в вашем тексте:',let_count)
+        other_symbol_count += 1
+print('Колличество цифр в вашем тексте:', num_count)
+print('Колличество букв в вашем тексте:', let_count)
 print('Колличество символов, не отнощясимся к буквам и цифрам в вашем тексте:', other_symbol_count)
